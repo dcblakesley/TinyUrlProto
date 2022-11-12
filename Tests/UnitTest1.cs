80e988268bdb9b7fb84155fc846ac09d2c7eb12f
David Blakesley <dcblakesley@gmail.com>
2022-11-11 21:42:48 -0700

    _

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -2,17 +2,61 @@ using TinyUrlProto;
 
 namespace Tests;
 
+
 [TestClass]
-public class Tests
+public class DemoTest
 {
-    readonly TinyUrlRepository _tinyUrlRepository;
-    readonly Guid _customerId;
-    public Tests()
+    readonly Guid _userId = Guid.Parse("9B978B0D-902E-47A5-B863-B00E0AB0D7E2");
+
+    public async Task Integration()
     {
-        _tinyUrlRepository = new TinyUrlRepository(new Database());
-        _customerId = Guid.Parse("9B978B0D-902E-47A5-B863-B00E0AB0D7E2");
+        // Typically these parts would be setup through the IOC framework in Asp.net or some other framework
+        var repo = new TinyUrlRepository(new Database());
+        var service = new TinyUrlService(repo);
+        
+        await Seed(service);
+
+        // 
+
+        // Act
+        // Assert
     }
 
+    /// <summary> Seed through the service, adds TinyUrls for anonymous users and customers </summary>
+    async Task Seed(TinyUrlService tinyUrlService)
+    {
+        // Only seeding valid TinyUrls, testing invalid ones is handled by the Unit Tests
+        List<TinyUrl> tinyUrlsToAdd = new()
+        {
+            // Anonymous Users
+            new("www.yahoo.com", "TinyUrl", null),
+            new("yahoo.com", "TinyUrl", null),
+            new("a.com", "TinyUrl", null),
+            new("google.com", "TinyUrl", null),
+            new("tomshardware.com", "CustomDomain", null),
+
+            // Customer
+            new("www.yahoo.com", "TinyUrl", _userId),
+            new("yahoo.com", "TinyUrl", _userId),
+            new("a.com", "TinyUrl", _userId),
+            new("google.com", "TinyUrl", _userId),
+            new("tomshardware.com", "CustomDomain", _userId)
+        };
+
+        foreach (var tinyUrl in tinyUrlsToAdd)
+        {
+            _ = await tinyUrlService.CreateTinyUrl(tinyUrl.UserId, tinyUrl);
+        }
+    }
+
+    async Task 
+}
+
+
+[TestClass]
+public class TinyUrlServiceTests
+{
+    readonly Guid _userId = Guid.Parse("9B978B0D-902E-47A5-B863-B00E0AB0D7E2");
 
     [TestMethod]
 
@@ -31,32 +75,24 @@ public class Tests
     [DataRow("yahoo.l", "TinyUrl", true, true, "")]
     [DataRow("yahoo.l", "Tiny Url", true, false, "Invalid character 'space' in domain name")]
     [DataRow("yah oo.l", "TinyUrl", true, false, "Invalid character 'space' in url")]
-    public void Create_TinyUrl(string longUrl, string domain, bool useCustomer, bool expectToPass, string explanationIfNotExpectedToPass)
+    public async Task Create_TinyUrl(string longUrl, string domain, bool useUser, bool expectToPass, string explanationIfNotExpectedToPass)
     {
         // Arrange
-        Guid? userId = null;
-        if (useCustomer)
-            userId = _customerId;
+        var repo = new TinyUrlRepository(new Database());
+        var service = new TinyUrlService(repo);
+        var tinyUrl = new TinyUrl(longUrl, domain);
+
+        TinyUrl? result = null;
 
         // Act
-        var tinyUrl = _tinyUrlRepository.CreateTinyUrl(userId, longUrl, domain);
+        result = useUser 
+            ? await service.CreateTinyUrl(_userId, tinyUrl) 
+            : await service.CreateTinyUrl(null, tinyUrl);
 
         // Assert
         if (expectToPass)
-            Assert.IsNotNull(tinyUrl);
+            Assert.IsNotNull(result);
         else
-            Assert.IsNull(tinyUrl);
+            Assert.IsNull(result);
     }
-    
-    public void Delete_TinyUrl()
-    {
-        // Add some
-    }
-
-
-
-
-
-
 }
-
--- TinyUrlProto/TinyUrlRepository.cs
+++ TinyUrlProto/TinyUrlRepository.cs
@@ -1,9 +1,12 @@
 ﻿namespace TinyUrlProto;
+#pragma warning disable CS1998 // using async to mimic behavior of using real storage
 
+/// <summary> Handles database operations, ensure TinyUrls are valid before trying to insert </summary>
 public class TinyUrlRepository
 {
-    readonly Database _database;
-    public TinyUrlRepository(Database database) => _database = database;
+    public readonly Database _database;
+    public TinyUrlRepository(Database database) => _database = database; // Also inject a Logger
+
 
     // Necessary 
     //  - Create short URLs with associated long URLs.
@@ -15,42 +18,88 @@ public class TinyUrlRepository
     /// A TinyUrl object if successful. <br/>
     /// null if there was an error such as validation failed.
     /// </returns>
-    public TinyUrl? CreateTinyUrl(Guid? userId, string newLongUrl, string desiredDomain)
+    public async Task<TinyUrl?> Add(TinyUrl tinyUrl)
     {
         User? user = null;
-        if (userId != null)
-            user = _database.UsersTable.FirstOrDefault(x => x.Id == userId);
+        if (tinyUrl.UserId != null)
+            user = _database.UsersTable.FirstOrDefault(x => x.Id == tinyUrl.UserId);
         
         // If the user isn't null, check if the longUrl already exists for that user
         if (user != null)
         {
-            var existingTinyUrl = _database.TinyUrlsTable.FirstOrDefault(x => x.UserId == userId && x.LongUrl == newLongUrl);
+            var existingTinyUrl = _database.TinyUrlsTable.FirstOrDefault(x => x.UserId == tinyUrl.UserId && x.LongUrl == tinyUrl.LongUrl);
             if (existingTinyUrl != null)
                 return existingTinyUrl;
         }
 
-        // No existing user or entity, generate a new one
-        var tinyUrl = new TinyUrl(GenerateId(), userId, newLongUrl, desiredDomain);
+        // No existing entity, generate a new Id
+        tinyUrl.Id = GenerateId();
 
-        // Check for problems before inserting
-        if (tinyUrl.Validate() != null)
-            return null;
-        
         // Add to Db and send back to Customer
         _database.TinyUrlsTable.Add(tinyUrl);
         return tinyUrl;
     }
 
-    public bool DeleteTinyUrl(Guid? userId, string tinyUrlId)
+    public async Task<bool> Delete(Guid? userId, string tinyUrlId)
     {
+        var target = _database.TinyUrlsTable.FirstOrDefault(x => x.Id == tinyUrlId);
+        
+        if (target == null) // Log user trying to delete a tinyUrl that doesn't exist
+            return false;
 
-
-        return true;
+        if(target.UserId != userId) // Log user does not have permission
+            return false;
+        
+        return _database.TinyUrlsTable.Remove(target);
     }
 
-    // Methods
+    /// <summary> Add Usages/Clicks to the TinyUrl </summary>
+    /// <param name="tinyUrlId">The 8 digit TinyUrl.Id</param>
+    /// <param name="count">The number of clicks to add to the Count, this can be more than 1 to allow batch adds</param>
+    /// <returns></returns>
+    public async Task AddClicks(string tinyUrlId, int count)
+    {
+        var tinyUrl = _database.TinyUrlsTable.FirstOrDefault(x => x.Id == tinyUrlId);
+        
+        if (tinyUrl == null) // Log user trying access tinyUrl that doesn't exist 
+            return;
+
+        tinyUrl.UseCount += count;
+    }
+
+    public async Task<string?> GetLongUrl(string tinyUrlId) => _database.TinyUrlsTable.FirstOrDefault(x => x.Id == tinyUrlId)?.LongUrl;
+    
 
     /// <remarks> Does not check for collisions which have a 1 in 2.82 trillion possibility </remarks>
     static string GenerateId() => Guid.NewGuid().ToString("N")[..8];
     public string? GetLongUrlFromTinyUrlString(string tinyUrl) => _database.TinyUrlsTable.FirstOrDefault(x => x.Id == tinyUrl)?.LongUrl;
+}
+
+/// <summary> Handles Validation and any other necessary work before sending the request to the Repository or Database </summary>
+public class TinyUrlService
+{
+    readonly TinyUrlRepository _tinyUrlRepository;
+    public TinyUrlService(TinyUrlRepository tinyUrlRepository) => _tinyUrlRepository = tinyUrlRepository;
+
+    /// <returns>
+    /// A TinyUrl object if successful. <br/>
+    /// null if there was an error such as validation failed.
+    /// </returns>
+    public async Task<TinyUrl?> CreateTinyUrl(Guid? userId, TinyUrl tinyUrl)
+    {
+        // Check for problems before inserting
+        if (tinyUrl.Validate() != null)
+            return null;
+
+        // Always get the Users' Id from the Authentication Provider
+        tinyUrl.UserId = userId;
+
+        // Add to Db and send back to Customer
+        return await _tinyUrlRepository.Add(tinyUrl);
+    }
+
+    public async Task<bool> DeleteTinyUrl(Guid? userId, string tinyUrlId) => await _tinyUrlRepository.Delete(userId, tinyUrlId);
+    
+    /// <remarks>Note: This is low priority, so if we have times where there are bursts too high for the hardware, add a caching mechanism to keep this from affecting adding links</remarks>>
+    public async Task AddClick(string tinyUrlId) => await _tinyUrlRepository.AddClicks(tinyUrlId, 1);
 }
